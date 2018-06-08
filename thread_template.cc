bd0552259593225dfad1581bb99a155893bc55c1
redfishleo <redfish.tbc@gmail.com>
2018-06-08 17:29:14 +0800

    Create thread_template.cc

---
+++ thread_template.cc
@@ -0,0 +1,20 @@
+//Create a C++11 thread from the main program
+
+#include <iostream>
+#include <thread>
+
+//This function will be called from a thread
+
+void thread_func() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main() {
+    //Launch a thread
+    std::thread t1(thread_func);
+
+    //Join the thread with the main thread
+    t1.join();
+    
+    return 0;
+}
